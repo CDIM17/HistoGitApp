020cedf6f26b4a74294dbe206756b5b60b07acf6
CDIM17 <56546970+CDIM17@users.noreply.github.com>
2023-10-07 23:43:35 -0400

    Implement Providers: Interceptors, Pipes and Filters

--- histo-git-backend/src/common/common.module.ts
+++ histo-git-backend/src/common/common.module.ts
@@ -1,4 +1,35 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { ValidationPipe } from './pipes/validation.pipe';
+import { TransformDataInterceptor } from './interceptors/transform-data.interceptor';
 
-@Module({})
-export class CommonModule {}
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformDataInterceptor,
+    },
+  ],
+  exports: [],
+})
+export class CommonModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
