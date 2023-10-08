2b1109c925d49509b2a7e89fb57fd29876675306
CDIM17 <56546970+CDIM17@users.noreply.github.com>
2023-10-08 15:34:46 -0400

    Create Structure for Frontend Project in Angular

---
+++ histo-git-frontend/src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommitHistoryComponent } from './commit-history/commit-history.component';
+
+@NgModule({
+  declarations: [
+    CommitHistoryComponent
+  ],
+  imports: [CommonModule],
+})
+export class PagesModule {}
